9669ed92bbe3ca423e5dfa7a11839862d98ae38d
Noah Holloway <noahholloway23@gmail.com>
2025-10-23 11:29:34 -0600

    Add project files.

---
+++ sendfrompic/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("sendfrompic")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Razer")>
+<Assembly: AssemblyProduct("sendfrompic")>
+<Assembly: AssemblyCopyright("Copyright © Razer 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c2921ba4-a085-4b33-8021-596f4f3cd3c8")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ sendfrompic/sendfrompic.vb
@@ -0,0 +1,3 @@
+﻿Public Class sendfrompic
+
+End Class
